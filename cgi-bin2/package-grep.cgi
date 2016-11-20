ekhavana/cygwin

commit dd2c1ad438f40ad57417922bdfa8d8eb9c37cd61

Replace some uses of <br> with </p><p>
Some of the remaining <br> are compensating for 0 margin set globally, which
could be done better...

cgi-bin2/package-grep.cgi
@@ -85,14 +85,18 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     cat ../top.html
     echo '<h1>Cygwin Package Search</h1>
 <form method="GET" action="//cygwin.com/cgi-bin2/package-grep.cgi">
+<p>
 Search package contents for a
 <a href="https://www.gnu.org/software/grep/manual/grep.html">grep</a>
 <a href="https://www.gnu.org/software/grep/manual/grep.html#Basic-vs-Extended">basic</a>
 <a href="https://en.wikipedia.org/wiki/Regular_expression">regular expression</a>
 pattern, or view the <a href="https://cygwin.com/packages/package_list.html">full list</a>
-of packages<br>
+of packages
+</p>
+<p>
 <input type="text" size=40 name="grep" value="'$param_grep_htmlencode'">
-<input type=submit value="Go"><br>'
+<input type=submit value="Go">'
+</p>
     echo '<input type="radio" name="arch" value="x86" '
     if [ "$param_arch" = "x86" ]; then echo 'checked="checked"'; fi
     echo '>x86'

git.html
@@ -57,7 +57,8 @@ git clone sourceware.org:/git/newlib-cygwin.git
   <p>The cygwin.com website is under Git control, too.  You can view the
   website Git repo via
   <a href="git/gitweb.cgi?p=cygwin-htdocs.git">its gitweb interface</a>.
-  <br>
+  </p>
+  <p>
   Note that this is only the framework.
   <a href="git/gitweb.cgi?p=newlib-cygwin.git;a=tree;f=winsup/doc">User's Guide, API and FAQ</a>
   are maintained as part of the aforementioned Cygwin source Git repository.
@@ -68,7 +69,8 @@ git clone sourceware.org:/git/newlib-cygwin.git
 
 <p>Please refer to the <a href="faq/faq.html#faq.programming.building-cygwin">
 FAQ</a>
-for information on building Cygwin.<br>Hint: You need to configure and
+for information on building Cygwin.</p>
+<p>Hint: You need to configure and
 build in a separate directory from the source.</p>
 
 </div>

commit b037ebeb61dcdebe9207553eecdf6f9f53265f3d

Fix quoting typo
Fix typo with quoting introduced in dd2c1ad4

cgi-bin2/package-grep.cgi
@@ -95,8 +95,8 @@ of packages
 </p>
 <p>
 <input type="text" size=40 name="grep" value="'$param_grep_htmlencode'">
-<input type=submit value="Go">'
-</p>
+<input type=submit value="Go">
+</p>'
     echo '<input type="radio" name="arch" value="x86" '
     if [ "$param_arch" = "x86" ]; then echo 'checked="checked"'; fi
     echo '>x86'